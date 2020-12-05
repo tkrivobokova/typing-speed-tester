75d1bfa67a57b55c79c68e4e44da130cbc9157ed
tkrivobokova <tkrivobokova@gmail.com>
2020-12-05 13:05:08 +0200

    added timer functionality

--- script.js
+++ script.js
@@ -4,20 +4,45 @@ const originText = document.querySelector("#origin-text p").innerHTML;
 const resetButton = document.querySelector("#reset");
 const theTimer = document.querySelector(".timer");
 
+let timer = [0, 0, 0, 0];
 
 // Add leading zero to numbers 9 or below (purely for aesthetics):
 
 
 // Run a standard minute/second/hundredths timer:
+function runTimer() {
+  let currentTime = timer[0] + ':' + timer[1] + ':' + timer[2];
+  theTimer.innerHTML = currentTime;
+  timer[3]++;
 
+  timer[0] = Math.floor((timer[3]/100)/60);
+  timer[1] = Math.floor((timer[3]/100) - (timer[0] * 60));
+  timer[2] = Math.floor(timer[3] - (timer[1] * 100) - (timer[0] * 6000));
+}
 
 // Match the text entered with the provided text on the page:
+function spellCheck() {
+  let enteredText = testArea.value;
+  console.log('enteredText: ', enteredText);
 
+}
 
 // Start the timer:
+function start() {
+  let enteredTextLenght = testArea.value.length;
+  if (enteredTextLenght === 0) {
+    setInterval(runTimer, 10);
+  }
+  console.log('enteredTextLenght: ', enteredTextLenght);
 
+}
 
 // Reset everything:
-
+function reset() {
+  console.log('reset');
+}
 
 // Event listeners for keyboard input and the reset button:
+testArea.addEventListener('keypress', start, false);
+testArea.addEventListener('keyup', spellCheck, false);
+resetButton.addEventListener('click', reset, false);
